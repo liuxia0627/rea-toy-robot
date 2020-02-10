454769a184477abf6be305433dbcc76916d1a57c
liu xia <xia.liu627@gmail.com>
2020-02-10 12:25:12 +1100

    Create and commit InputArgs and CommandParser classes: InputArgs.java - Implements functional interface Function<String[], Stream<String>> to accept input command args and return the Stream of command Strings in 2 scenarios: 1. Specified file path defined within command args - read file and return a Steam of command Strings based on the file path 2. No Specified file path defined within command args - read input from stdin and return a steam of command Strings CommandParser.java - Implement functional interface Function<String, Command> to parse a command String into a Command object.

---
+++ src/main/java/com/reagroup/toyrobot/model/args/CommandParser.java
@@ -0,0 +1,124 @@
+package com.reagroup.toyrobot.model.args;
+
+import com.reagroup.toyrobot.model.command.*;
+import com.reagroup.toyrobot.model.state.Orientation;
+import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+
+import static com.reagroup.toyrobot.constants.Constants.*;
+
+/**
+ * Parses a command String into a command object
+ */
+
+
+public class CommandParser implements Function<String, Command>  {
+    private final static Logger LOGGER = Logger.getLogger(CommandParser.class.getName());
+
+
+    /**
+     * Parses a command String to a command Object
+     * @param commandString input command string
+     * @return parsed executable command object
+     */
+    @Override
+    public Command apply(String commandString) {
+        if (commandString == null || commandString.isEmpty()) {
+            LOGGER.warning("Command cannot be empty");
+            return new NullCommand();
+        } else if (commandString.equals(LEFT_COMMAND)) {
+            return new LeftCommand();
+        } else if (commandString.equals(MOVE_COMMAND)) {
+            return new MoveCommand();
+        } else if (commandString.startsWith(PLACE_COMMAND)) {
+            return this.parsePlaceCommand(commandString);
+        } else if (commandString.equals(REPORT_COMMAND)) {
+            return new ReportCommand();
+        } else if (commandString.equals(RIGHT_COMMAND)) {
+            return new RightCommand();
+        } else if (commandString.equals(TAKEOFF_COMMAND)) {
+            return new TakeoffCommand();
+        }
+        else {
+            LOGGER.warning("Invalid command: " + commandString + "; doing nothing.");
+            return new NullCommand();
+        }
+    }
+
+    /**
+     * Parses place command String to a executable place command object
+     * @param commandString place command String
+     * @return
+     */
+    private Command parsePlaceCommand(String commandString) {
+        Matcher matcher = PLACE_REGEX.matcher(commandString);
+        if(matcher.matches()){
+            return createPlaceCommand(matcher);
+        } else {
+            LOGGER.warning("Invalid command: " + commandString + "; doing nothing.");
+            return new NullCommand();
+        }
+    }
+
+    /**
+     * Create place command based on matched place command String args
+     * @param matcher matched place command string args
+     * @return executable place command object
+     */
+    private Command createPlaceCommand (Matcher matcher) {
+    try{
+        return new PlaceCommand(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), this.parsePlaceCommandOrientation(matcher.group(3)));
+    } catch (IllegalArgumentException ex){
+        LOGGER.warning(ex.getMessage());
+        return new NullCommand();
+    }
+
+    }
+
+    /**
+     * Parse and get the orientation based on matched place command string args
+     * @param orientationString matched orientation command string args
+     * @return  orientation of the place command object
+     */
+    private Orientation parsePlaceCommandOrientation(String orientationString) {
+        if(orientationString == null || orientationString.isEmpty()){
+            throw new IllegalArgumentException("Orientation cannot be empty");
+        }
+        switch (orientationString){
+            case ORIENTATION_NORTH:
+                return Orientation.NORTH;
+            case ORIENTATION_EAST:
+                return Orientation.EAST;
+            case ORIENTATION_SOUTH:
+                return Orientation.SOUTH;
+            case ORIENTATION_WEST:
+                return Orientation.WEST;
+            default:
+                throw new IllegalArgumentException("Invalid Orientation: " + orientationString);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/reagroup/toyrobot/model/args/InputArgs.java
@@ -0,0 +1,66 @@
+package com.reagroup.toyrobot.model.args;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+/**
+ * This class provides API to reads the file provided as parameter or reads the input command from stdin of no file defined
+ * The input is composed as a stream of strings, and each represents a command string which will be finally parsed into a command object
+ */
+
+public class InputArgs implements Function<String[], Stream<String>> {
+
+    private final static Logger LOGGER = Logger.getLogger(InputArgs.class.getName());
+
+    @Override
+    public Stream<String> apply(String[] args) {
+        switch (args.length){
+            case 0:
+                return stdinStream();
+            case 1:
+                return fileStream(args[0]);
+            default:
+                LOGGER.severe("Invalid arguments. Must be either blank or one filename.");
+                return Stream.empty();
+        }
+    }
+
+    /**
+     * Reads lines of the files and return a stream of command Strings
+     * @param filePath Path of file
+     * @return Stream of Strings, each represents a command String which will be finally parsed to a command object
+     */
+    private Stream<String> fileStream(String filePath){
+        try{
+            return Files.lines(Paths.get(filePath));
+        }catch(IOException ex){
+            LOGGER.severe("There is IOException Occurred: " + ex.getMessage());
+            return Stream.empty();
+        }
+    }
+
+    /**
+     * Reads input from stdin and return a stream of command Strings
+     * @return a stream of command Strings which will finally be parsed to command objects
+     */
+    private Stream<String> stdinStream(){
+        Scanner scanner = new Scanner(System.in);
+        List<String> result = new ArrayList<>();
+
+        System.out.println("Please enter the proper commands to start Toy Robot Simulator");
+        System.out.println("Command line should be entered per line.");
+        System.out.println();
+
+        while(scanner.hasNext()){
+            result.add(scanner.nextLine());
+        }
+        return result.stream();
+    }
+}
